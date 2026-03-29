97aeb0f39e6b401cbb6bfa0e95a5c4561ac236a2
maayan2299 <154839882+maayan2299@users.noreply.github.com>
2026-03-29 11:52:45 +0300

    Update CheckoutPage.jsx

--- nerlya2026-main/src/pages/CheckoutPage.jsx
+++ nerlya2026-main/src/pages/CheckoutPage.jsx
@@ -112,7 +112,8 @@ export default function CheckoutPage() {
       // --- תחילת מנגנון התשלום הישיר ---
       const form = document.createElement('form');
       form.method = 'POST';
-      form.action = 'https://pay.yaad.net/p/';
+      form.action = 'https://icom.yaad.net/p/';
+      
 
       const fields = {
         action: 'pay',
